6f4d547c3aab373462f041c4d5820c4b97f28785
joelnune <91698993+joelnune@users.noreply.github.com>
2022-05-20 22:25:06 -0300

    Add files via upload

---
+++ Cadastro-Cliente/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs')
+var path = require('path');
+app.set('views', path.join(__dirname, '/view/'));
+
+bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true}));
+
+var consign = require('consign');
+consign().include('controller/routes',).into(app);
+
+app.listen(8081, function(){
+    console.log("Servidor funcionando na url http://localhost:8081");
+})
